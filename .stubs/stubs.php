e1d10d6e85a719a490e1f84f2e769a57f1779e12
DarkGhosthunter <DarkGhostHunter@gmail.com>
2022-02-19 14:43:16 -0300

    Stricter stubs.

--- .stubs/stubs.php
+++ .stubs/stubs.php
@@ -26,3 +26,30 @@ namespace Illuminate\Database\Query {
         }
     }
 }
+
+namespace Illuminate\Database\Eloquent {
+
+    use DateInterval;
+    use DateTimeInterface;
+
+    class Builder
+    {
+        /**
+         * Caches the underlying query results.
+         *
+         * @param  \DateTimeInterface|\DateInterval|int|null  $ttl
+         * @param  string  $key
+         * @param  string|null  $store
+         * @param  int  $wait
+         * @return static
+         */
+        public function cache(
+            DateTimeInterface|DateInterval|int|null $ttl = null,
+            string $key = '',
+            string $store = null,
+            int $wait = 0
+        ): static {
+            //
+        }
+    }
+}
